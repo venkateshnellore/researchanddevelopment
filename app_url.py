13af342068f5a61e436c6cc3b0b36905a97c33a0
venkateshnellore <44094182+venkateshnellore@users.noreply.github.com>
2018-10-12 20:39:36 +0530

    Update app_url.py

--- app_url.py
+++ app_url.py
@@ -2,9 +2,7 @@ import json
 from flask import Flask,request, jsonify
 from flask_cors import CORS
 from otp_send import sendotp
-from otp_verify import verifyotp
-from send_sms import sendsms
-from pdf import generatepdf
+
 app = Flask(__name__)
 CORS(app)
 
@@ -13,21 +11,8 @@ def hello():
    return ("Application is working Fine")
 
 @app.route('/sendotp',methods=['POST'])
-def indexotp():
+def sendotp():
    return sendotp(request)
-
-@app.route('/optverification',methods=['POST'])
-def verify():
-   return verifyotp(request)
-
-@app.route('/generatesms',methods=['POST'])
-def sendSMS():
-   return sendsms(request)
-
-@app.route('/generatepdf',methods=['GET'])
-def sendPDF():
-   return generatepdf(request)
-
 
 if __name__ == "__main__":
   app.run(debug=True)
